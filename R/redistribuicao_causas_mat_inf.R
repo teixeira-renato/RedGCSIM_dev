fdb2257abcefab198ddcc39c6acfe45a06313c80
AdemarDantas <72451366+AdemarDantas@users.noreply.github.com>
2025-12-26 14:25:52 -0300

    Update redistribuicao_causas_mat_inf.R
    Uso de uma função padrão para sistematizar os cálculos de propoções e redistribuição.

--- R/redistribuicao_causas_mat_inf.R
+++ R/redistribuicao_causas_mat_inf.R
@@ -38,213 +38,32 @@
 redistribuicao_causas_mat_inf = function (dados_completos, dados_redis){
   if (!require("pacman")) install.packages("pacman") #garantir que o pacman está instalado
   pacman::p_load(tidyverse) # pacotes necessários
-
+  
   causas=unique(ICD$CLASS_GPEAS_PRODUCAO)[!grepl(pattern = "^_",x = unique(ICD$CLASS_GPEAS_PRODUCAO))]
   causas=c(causas,"_pneumo")
   
-  ##### Óbito Materno----
-
-  mat <-c(causas[grepl("^materna",causas)])
-
-  base.5 <- dados_completos %>%
-    select(!(pr.mu:ob.rg),-redis,-redis.2, -redis.3,-redis.4,-c.red) %>%
-    mutate(c.red=ifelse(GBD %in% mat & sexo == "Feminino" & idade%in%c("10","15","20","25","30","35","40","45","50"), '_maternas', NA)) %>%
+  causas_mat   <- causas[grepl("^materna", causas)]
+  causas_inf   <- causas[grepl("^infant|anom_congenitas|aspiracao_pulmunar|obst_intestinal|lri_post_neo", causas)]
+  
+  idades_mat   <- c("10","15","20","25","30","35","40","45","50")
+  idades_inf   <- c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year")
+  
+  base_final <- dados_completos %>%
+    select(-starts_with("pr."), -starts_with("ob."), 
+           -redis, -redis.2, -redis.3, -redis.4, -c.red) %>%
+    mutate(c.red=ifelse(GBD %in% causas_mat & sexo == "Feminino" & idade%in%c("10","15","20","25","30","35","40","45","50"), '_maternas', NA)) %>%
     left_join(dados_redis, by=c('cdmun','micro','meso',  'ano', 'sexo','idade', 'uf', 'c.red'))
-
-  ###Proporções mat
-
-  ###PRMUN
-  muni.mat <- base.5 %>% 
-    filter(GBD %in% mat & sexo == "Feminino") %>% 
-    filter(idade%in%c("10","15","20","25","30","35","40","45","50"))%>%
-    group_by(cdmun,micro,meso, GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.7, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(cdmun,micro,meso,idade, ano, sexo, uf) %>% 
-    mutate(pr.mu=ob/sum(ob,na.rm=T),
-           ob.mu=sum(ob,na.rm=T)) %>% 
-    select(-ob)
   
-  ###PR.MICRO 
-  micro.mat <- base.5 %>% 
-    filter(GBD %in% mat & sexo == "Feminino") %>% 
-    filter(idade%in%c("10","15","20","25","30","35","40","45","50"))%>%
-    group_by(micro,meso, GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.7, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(micro,meso,idade, ano, sexo, uf) %>% 
-    mutate(pr.mi=ob/sum(ob,na.rm=T),
-           ob.mi=sum(ob,na.rm=T)) %>% 
-    select(-ob)
+  base_final <- calc_props(base = base_final, causa = causas_mat, prefix = "mat", obito_in = "obitos.7", obito_out = "obitos.8",
+                           sexo_filtro = "Feminino",idades_filtro = idades_mat)
   
-  ###PR.MESO 
-  meso.mat <- base.5 %>% 
-    filter(GBD %in% mat & sexo == "Feminino") %>% 
-    filter(idade%in%c("10","15","20","25","30","35","40","45","50"))%>%
-    group_by(meso, GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.7, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(meso,idade, ano, sexo, uf) %>% 
-    mutate(pr.me=ob/sum(ob, na.rm = T),
-           ob.me=sum(ob, na.rm = T)) %>% 
-    select(-ob)
-  
-  ###PR.UF
-  uf.mat <- base.5 %>%
-    filter(GBD %in% mat & sexo == "Feminino") %>% 
-    filter(idade%in%c("10","15","20","25","30","35","40","45","50"))%>%
-    group_by( GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.7, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(idade, ano, sexo, uf) %>% 
-    mutate(pr.uf=ob/sum(ob, na.rm = T),
-           ob.uf=sum(ob, na.rm = T)) %>% 
-    select(-ob)
-  
-  ###PR.REG
-  rg.mat <- base.5 %>%
-    filter(GBD %in% mat & sexo == "Feminino") %>% 
-    filter(idade%in%c("10","15","20","25","30","35","40","45","50"))%>%
-    group_by( GBD,idade, ano, sexo, reg) %>% 
-    summarise(ob=sum(obitos.7, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(idade, ano, sexo, reg) %>% 
-    mutate(pr.rg=ob/sum(ob,na.rm=T),
-           ob.rg=sum(ob,na.rm=T)) %>% 
-    select(-ob)
-  
-  base.5 <- base.5 %>% 
-    left_join(muni.mat, by=c('cdmun','micro','meso', 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(micro.mat, by=c('micro','meso', 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(meso.mat, by=c('meso', 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(uf.mat, by=c( 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(rg.mat, by=c( 'GBD','idade', 'ano', 'sexo', 'reg'))
-  
-  
-  base.5 <- base.5 %>% 
-    mutate(mat.1=(redis)*pr.mu,
-           redis.2=ifelse(is.na(mat.1) & ob.mu==0, redis,NA),
-           mat.2=(redis.2)*pr.mi,
-           redis.3=ifelse(is.na(mat.2) & ob.mi==0, redis.2,NA),
-           mat.3=(redis.3)*pr.me,
-           redis.4=ifelse(is.na(mat.3) & ob.me==0, redis.3,NA),
-           mat.4=(redis.4)*pr.uf,
-           redis.5=ifelse(is.na(mat.4) & ob.uf==0, redis.4,NA),
-           mat.5=(redis.5)*pr.rg,
-           obitos.8= ifelse(!is.na(mat.1), obitos.7+mat.1, 
-                            ifelse(!is.na(mat.2), obitos.7+mat.2,
-                                   ifelse(!is.na(mat.3), obitos.7+mat.3,
-                                          ifelse(!is.na(mat.4), obitos.7+mat.4,
-                                                 ifelse(!is.na(mat.5), obitos.7+mat.5, obitos.7))))))
-  
-  #validação
-
-  # obitos_para_redis <- sum(base.r[grepl('_maternas',base.r$c.red),]$redis, na.rm = T)
-  # obitos_pre_redis <- sum(base.5$obitos.7,na.rm = T)
-  # round(sum(base.5$obitos.8,na.rm = T))==round(sum(obitos_para_redis,obitos_pre_redis))
-
-  ##### Óbito Neonatal----
-
-  infant <-c(causas[grepl("^infant|anom_congenitas|aspiracao_pulmunar|obst_intestinal|lri_post_neo",causas)])
-
-  base.5 <- base.5 %>%
-    select(!(pr.mu:ob.rg),-redis,-redis.2, -redis.3,-redis.4,-c.red) %>%
-    mutate(c.red=ifelse(GBD %in% infant & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year"), '_infant_neonat', NA)) %>%
+  base_final <- base_final %>%
+    select(-starts_with("pr."), -starts_with("ob."), 
+           -redis, -redis.2, -redis.3, -redis.4, -c.red) %>%
+    mutate(c.red=ifelse(GBD %in% causas_inf & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year"), '_infant_neonat', NA)) %>%
     left_join(dados_redis, by=c('cdmun','micro','meso',  'ano', 'sexo','idade', 'uf', 'c.red'))
-
-  ###Proporções infant
-
-  ###PRMUN
-  muni.infant <- base.5 %>% 
-    filter(GBD %in% infant & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year")) %>% 
-    group_by(cdmun,micro,meso, GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.8, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(cdmun,micro,meso,idade, ano, sexo, uf) %>% 
-    mutate(pr.mu=ob/sum(ob,na.rm=T),
-           ob.mu=sum(ob,na.rm=T)) %>% 
-    select(-ob)
   
-  ###PR.MICRO 
-  micro.infant <- base.5 %>% 
-    filter(GBD %in% infant & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year")) %>% 
-    group_by(micro,meso, GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.8, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(micro,meso,idade, ano, sexo, uf) %>% 
-    mutate(pr.mi=ob/sum(ob,na.rm=T),
-           ob.mi=sum(ob,na.rm=T)) %>% 
-    select(-ob)
+  base_final <- calc_props(base = base_final, causa = causas_inf, prefix = "infant", obito_in = "obitos.8", obito_out = "obitos.9",sexo_filtro = NULL,idades_filtro = idades_inf)
   
-  ###PR.MESO 
-  meso.infant <- base.5 %>% 
-    filter(GBD %in% infant & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year")) %>% 
-    group_by(meso, GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.8, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(meso,idade, ano, sexo, uf) %>% 
-    mutate(pr.me=ob/sum(ob, na.rm = T),
-           ob.me=sum(ob, na.rm = T)) %>% 
-    select(-ob)
-  
-  ###PR.UF
-  uf.infant <- base.5 %>%
-    filter(GBD %in% infant & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year")) %>% 
-    group_by( GBD,idade, ano, sexo, uf) %>% 
-    summarise(ob=sum(obitos.8, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(idade, ano, sexo, uf) %>% 
-    mutate(pr.uf=ob/sum(ob, na.rm = T),
-           ob.uf=sum(ob, na.rm = T)) %>% 
-    select(-ob)
-  
-  ###PR.REG
-  rg.infant <- base.5 %>%
-    filter(GBD %in% infant & idade %in% c("Early Neonatal","Post Neonatal","Late Neonatal","<1 year")) %>% 
-    group_by( GBD,idade, ano, sexo, reg) %>% 
-    summarise(ob=sum(obitos.8, na.rm = T))%>% 
-    ungroup() %>% 
-    group_by(idade, ano, sexo, reg) %>% 
-    mutate(pr.rg=ob/sum(ob,na.rm=T),
-           ob.rg=sum(ob,na.rm=T)) %>% 
-    select(-ob)
-  
-  base.5 <- base.5 %>% 
-    left_join(muni.infant, by=c('cdmun','micro','meso', 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(micro.infant, by=c('micro','meso', 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(meso.infant, by=c('meso', 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(uf.infant, by=c( 'GBD','idade', 'ano', 'sexo', 'uf')) 
-  base.5 <- base.5%>% 
-    left_join(rg.infant, by=c( 'GBD','idade', 'ano', 'sexo', 'reg'))
-  
-  
-  base.5 <- base.5 %>% 
-    mutate(infant.1=(redis)*pr.mu,
-           redis.2=ifelse(is.na(infant.1) & ob.mu==0, redis,NA),
-           infant.2=(redis.2)*pr.mi,
-           redis.3=ifelse(is.na(infant.2) & ob.mi==0, redis.2,NA),
-           infant.3=(redis.3)*pr.me,
-           redis.4=ifelse(is.na(infant.3) & ob.me==0, redis.3,NA),
-           infant.4=(redis.4)*pr.uf,
-           redis.5=ifelse(is.na(infant.4) & ob.uf==0, redis.4,NA),
-           infant.5=(redis.5)*pr.rg,
-           obitos.9= ifelse(!is.na(infant.1), obitos.8+infant.1, 
-                            ifelse(!is.na(infant.2), obitos.8+infant.2,
-                                   ifelse(!is.na(infant.3), obitos.8+infant.3,
-                                          ifelse(!is.na(infant.4), obitos.8+infant.4,
-                                                 ifelse(!is.na(infant.5), obitos.8+infant.5, obitos.8))))))
-  
-  # validação
-  # obitos_para_redis <- sum(base.r[grepl('_infant_neonat',base.r$c.red),]$redis, na.rm = T) #TODO: Conferir
-  # obitos_pre_redis <- sum(base.5$obitos.8,na.rm = T)
-  # round(sum(base.5$obitos.9,na.rm = T))==round(sum(obitos_para_redis,obitos_pre_redis))
-  # round(sum(base.5$obitos.9,na.rm = T))-round(sum(obitos_para_redis,obitos_pre_redis))
-  return(base.5)
+  return(base_final)
 }
